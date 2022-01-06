7a971eaf8dd0ec006157c388aebadc65f844c4b5
tatianaAcn24 <tatiana.acn24@gmail.com>
2022-01-06 18:54:26 -0300

    Agrego modificaciones

--- FontTech/app.js
+++ FontTech/app.js
@@ -12,9 +12,9 @@ app.get('/', (req,res)=>{
     res.sendFile(path.resolve('./views/home.html'));
 });
 
- app.get('/login', (req,res)=>{
+/*  app.get('/login', (req,res)=>{
     res.sendFile(path.resolve('./views/login.html'));
-});
+}); */
 app.get('/registro', (req,res)=>{
     res.sendFile(path.resolve('./views/registro.html'));
 });
